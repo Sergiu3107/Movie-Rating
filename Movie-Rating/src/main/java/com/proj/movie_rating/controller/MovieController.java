4bfaa08c3696ca7be87fef501d838b11d92cdea8
Sergiu3107 <isacsergiu96@gmail.com>
2024-05-24 17:39:01 +0300

    added cross to controllers

--- Movie-Rating/src/main/java/com/proj/movie_rating/controller/MovieController.java
+++ Movie-Rating/src/main/java/com/proj/movie_rating/controller/MovieController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping(path = "/movie")
 public class MovieController {
